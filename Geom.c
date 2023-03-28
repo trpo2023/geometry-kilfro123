f9a20014a9582dd4380bc88e6ba32bd1f1ae70cb
Dupin Artem <vip.dyupin@inbox.ru>
2023-03-28 09:58:29 +0700

    modified6: Geom.c

--- Geom.c
+++ Geom.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define RED_FLASH "\e[5;31m"
 #define END "\e[0m"
 #define MAX 100
 
@@ -38,43 +39,51 @@ void output_bugs(int errors, int num, char* ch)
     case BUG_NAME:
         printf("An error %d was found in the input line %s'Circle'%s\n",
                num,
+               RED_FLASH
                END);
         break;
     case BUG_STAPLES:
         if (*ch == ')') {
             printf("An error %d was found in the input line %s')'%s\n",
                    num,
+                   RED_FLASH
                    END);
             break;
         } else {
             printf("An error %d was found in the input line %s'('%s\n",
                    num,
+                   RED_FLASH
                    END);
             break;
         }
     case BUG_STAPLES_2:
         printf("An error %d was found in the input line %s')'%s\n",
                num,
+               RED_FLASH
                END);
         break;
     case BUG_NUMBER:
         printf("An error %d was found in the input line %s'double'%s\n",
                num,
+               RED_FLASH
                END);
         break;
     case BUG_UNIDENTIFIED_VARIABLES:
         printf("An error %d was found in the input line %s'variable'%s\n",
                num,
+               RED_FLASH
                END);
         break;
     case BUG_EXPECT_COMMA:
         printf("An error %d was found in the input line %s','%s\n",
                num,
+               RED_FLASH
                END);
         break;
     case BUG_EXTRA_POINT:
         printf("An error %d was found in the input line %s'.'%s\n",
                num,
+               RED_FLASH
                END);
         break;
     }
